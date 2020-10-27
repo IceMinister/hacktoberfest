459f0e6b7849f081708dc9518c28319511b940b8
IceMinister <73544807+IceMinister@users.noreply.github.com>
2020-10-27 22:13:41 +0530

    Hello World Program in Java

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+Language: Java
+Author: Kushal Aswani
+Github Link: https://github.com/IceMinister
+*/
+
+/*
+This program is used to display Hello World as an output
+*/
+
+class HelloWorld
+{
+ public static void main(String[] args)
+ {
+   System.out.println("Hello World");
+ }
+}
+
+  
